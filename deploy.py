1c68714576a5f9cfd49b3d2e94e849ad9dfac36c
ziausyed <ziausyed@cisco.com>
2018-04-03 08:51:57 -0700

    MVPZ: Deployment Script - User Input Validation Abstraction

--- deploy.py
+++ deploy.py
@@ -45,24 +45,39 @@ print("\n")
 
 
 ###########################################################################################################################
+### Input Processing
+def process_input(input):
+    if input in ('y', 'yes', 'Y', 'Yes'):
+        return True
+    elif input in ( 'n', 'no', 'N', 'No'):
+        return False
+    else:
+        raise ValueError('User entered invalid input.')
+###########################################################################################################################
+
+
+###########################################################################################################################
 ### Kafka Cluster Management
 ### Do they have a Kafka cluster or do we create one?
 while True:
-    kafka_exists = input("Do you have a Kafka cluster you would like to use (y/n): ")
-    if kafka_exists in ('y', 'yes', 'Y'):
+    kafka_input = input("Do you have a Kafka cluster you would like to use (y/n): ")
+    try:
+        kafka_exists = process_input(kafka_input)
+    except ValueError:
+        print("Please enter yes or no")
+        continue
+    if kafka_exists:
         kafka_endpoint = input("Please enter the Kafka endpoint (i.e. 10.200.99.44:30902): ")
         config['VOLTRON']['kafka_endpoint'] = kafka_endpoint
         ### VALIDATE INPUT HERE
         ### SET THEIR KAFKA ENDPOINT IN NECESSARY FILES HERE
         break
-    elif kafka_exists in ('n', 'no', 'N'):
+    elif not kafka_exists:
         print("No worries! We're setting up a Kafka cluster for you in OpenShift now.")
         kafka_endpoint = host_ip + ":30902"
         config['VOLTRON']['kafka_endpoint'] = kafka_endpoint
         ### SET UP KAFKA IN OPENSHIFT HERE
         break
-    else:
-        print("Please enter yes or no")
 
 for i in range(89):
     cprint("-", 'blue', end=' ')
@@ -73,21 +88,24 @@ print("\n")
 ### InfluxDB Management
 ### Do they have an Influx instance or do we create one?
 while True:
-    influx_exists = input("Do you have a pre-existing InfluxDB instance you would like to use (y/n): ")
-    if influx_exists in ('y', 'yes', 'Y'):
+    influx_input = input("Do you have a pre-existing InfluxDB instance you would like to use (y/n): ")
+    try:
+        influx_exists = process_input(influx_input)
+    except ValueError:
+        print("Please enter yes or no")
+        continue
+    if influx_exists:
         influx_endpoint = input("Please enter the InfluxDB endpoint (i.e. 10.200.99.44:30308): ")
         config['VOLTRON']['influx_endpoint'] = influx_endpoint
         ### VALIDATE INPUT HERE
         ### SET THEIR INFLUXDB ENDPOINT IN NECESSARY FILES HERE
         break
-    elif influx_exists in ('n', 'no', 'N'):
+    elif not influx_exists:
         print("No worries! We're setting up a InfluxDB instance for you in OpenShift now.")
         influx_endpoint = host_ip + ":30308"
         config['VOLTRON']['influx_endpoint'] = influx_endpoint
         ### SET UP INFLUXDB IN OPENSHIFT HERE
         break
-    else:
-        print("Please enter yes or no")
 
 for i in range(89):
     cprint("-", 'blue', end=' ')
@@ -98,21 +116,24 @@ print("\n")
 ### OpenBMP Management
 ### Do they have a pre-existing OpenBMP setup?
 while True:
-    openbmp_exists = input("Do you have a pre-existing OpenBMP setup you would like to use (y/n): ")
-    if openbmp_exists in ('y', 'yes', 'Y'):
+    openbmp_input = input("Do you have a pre-existing OpenBMP setup you would like to use (y/n): ")
+    try:
+        openbmp_exists = process_input(openbmp_input)
+    except ValueError:
+        print("Please enter yes or no")
+        continue
+    if openbmp_exists:
         openbmp_endpoint = input("Please enter the openbmp endpoint (i.e. 10.20.0.51:5000): ")
         config['VOLTRON']['openbmp_endpoint'] = openbmp_endpoint
         ### VALIDATE INPUT HERE
         ### SET THEIR OPENBMP ENDPOINT IN NECESSARY FILES HERE
         break
-    elif openbmp_exists in ('n', 'no', 'N'):
+    elif not openbmp_exists:
         print("No worries! We'll configure your routers to send OpenBMP data, and we'll get that data into the Kafka cluster now.")
         openbmp_endpoint = host_ip + ":5000"
         config['VOLTRON']['openbmp_endpoint'] = openbmp_endpoint
         ### SET UP OPENBMP HERE
         break
-    else:
-        print("Please enter yes or no")
 
 for i in range(89):
     cprint("-", 'blue', end=' ')
@@ -123,21 +144,24 @@ print("\n")
 ### ArangoDB Management
 ### Do they have an ArangoDB instance or do we create one?
 while True:
-    arango_exists = input("Do you have a pre-existing instance of ArangoDB you would like to use (y/n): ")
-    if arango_exists in ('y', 'yes', 'Y'):
+    arango_input = input("Do you have a pre-existing instance of ArangoDB you would like to use (y/n): ")
+    try:
+        arango_exists = process_input(arango_input)
+    except ValueError:
+        print("Please enter yes or no")
+        continue
+    if arango_exists:
         arango_endpoint = input("Please enter the ArangoDB endpoint (i.e. 10.200.99.44:30852): ")
         config['VOLTRON']['arango_endpoint'] = arango_endpoint
         ### VALIDATE INPUT HERE
         ### SET THEIR ARANGO ENDPOINT IN NECESSARY FILES HERE
         break
-    elif arango_exists in ('n', 'no', 'N'):
+    elif not arango_exists:
         print("No worries! We're setting up an ArangoDB instance for you in OpenShift now.")
         arango_endpoint = host_ip + ":30852"
         config['VOLTRON']['arango_endpoint'] = arango_endpoint
         ### SET UP ARANGO IN OPENSHIFT HERE
         break
-    else:
-        print("Please enter yes or no")
 
 for i in range(89):
     cprint("-", 'blue', end=' ')
@@ -220,5 +244,6 @@ with open(openbmpd_process_vars, "w") as file_handler:
     file_handler.write(outputText)
 
 ###########################################################################################################################
+
 
 
