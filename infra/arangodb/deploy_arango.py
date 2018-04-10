e7ab08a29b85ff6b906bcddfcb44d6339dad6dcf
ziausyed <ziausyed@cisco.com>
2018-04-10 03:31:17 -0400

    MVPZ: Infrastructure - Scripting Arango and Telemetry Deployment

--- infra/arangodb/deploy_arango.py
+++ infra/arangodb/deploy_arango.py
@@ -1,6 +1,28 @@
-### Script to deploy Arango
+import yaml, urllib3
+import openshift_deployer
+from openshift import client as openshift_client, config
+from kubernetes import client as kube_client, config as kube_config
 
-# OC Login
-# OC Project
-# OC apply everything in this directory
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+config.load_kube_config()
+deployment_api = openshift_client.AppsV1Api()
+stateful_set_api = openshift_client.AppsV1Api()
+service_api = kube_client.CoreV1Api()
+route_api = openshift_client.OapiApi()
+pod_api = kube_client.CoreV1Api()
+persistent_volume_api = kube_client.CoreV1Api()
+persistent_volume_claim_api = kube_client.CoreV1Api()
+config_map_api = kube_client.CoreV1Api()
+
+openshift_project = "voltron"
+# openshift_deployer.deploy_deployment(deployment_api, "deployment.yaml", openshift_project)
+# openshift_deployer.deploy_stateful_set(stateful_set_api, "stateful_set.yaml", openshift_project)
+openshift_deployer.deploy_service(service_api, "service.yaml", openshift_project)
+openshift_deployer.deploy_route(route_api, "route.yaml", openshift_project)
+# openshift_deployer.deploy_pod(pod_api, "pod.yaml", openshift_project)
+# openshift_deployer.deploy_persistent_volume(persistent_volume_api, "persistent_volume.yaml", openshift_project)
+# openshift_deployer.deploy_persistent_volume_claim(persistent_volume_claim_api, "persistent_volume_claim.yaml", openshift_project)
+# openshift_deployer.deploy_config_map(config_map_api, "config-map.yaml", openshift_project)
+
 
---
+++ infra/arangodb/deploy_authentication.py
@@ -0,0 +1,20 @@
+from openshift import client as openshiftclient, config as openshiftconfig
+
+#configuration = openshiftclient.Configuration()
+#configuration.api_key['authorization'] = 'c_iYoTxLV0AnygsUVmGfAE_xdcdjMhwRRncENY5yJkc'
+#configuration.api_key['authorization'] = 'po57qWjMrXa50HVBPzBsSsxn-ckiokytZkHY7ZMlAnw'
+#configuration.api_key_prefix['authorization'] = 'Bearer'
+#configuration.access_token = 'c_iYoTxLV0AnygsUVmGfAE_xdcdjMhwRRncENY5yJkc'
+#configuration.access_token = 'po57qWjMrXa50HVBPzBsSsxn-ckiokytZkHY7ZMlAnw'
+
+# create an instance of the API class
+#api_instance = openshiftclient.AppsOpenshiftIoV1Api(openshiftclient.ApiClient(configuration))
+#body = openshiftclient.V1DeploymentConfig() # V1DeploymentConfig |
+#pretty = 'pretty_example' # str | If 'true', then the output is pretty printed. (optional)
+
+#try:
+#    api_response = api_instance.create_deployment_config_for_all_namespaces(body, pretty=pretty)
+#    pprint(api_response)
+#except:
+#    print("Failed")
+
