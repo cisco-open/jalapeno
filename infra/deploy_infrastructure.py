71ee36a46db241a9b2857571ccfda855de57093f
ziausyed <ziausyed@cisco.com>
2018-04-13 03:21:40 -0400

    MVPZ: Infrastructure - Kafka, Arango, and OpenBMPD Deployment Automation

--- infra/deploy_infrastructure.py
+++ infra/deploy_infrastructure.py
@@ -2,8 +2,6 @@
 """Deploy Infrastructure including Kafka,
 Arango, Influx, OpenBMP, Telemetry, MDT.
 """
-from subprocess import call
-import pexpect, sys
 import kafka.deploy_kafka as KafkaDeployer
 import arangodb.deploy_arango as ArangoDeployer
 import influxdb.deploy_influx as InfluxDeployer
@@ -11,22 +9,17 @@ import openbmpd.deploy_openbmp as OpenbmpDeployer
 import telemetry.deploy_telemetry as TelemetryDeployer
 import mdt-stack.deploy_mdt as MDTDeployer
 
+def main():
+    """Deploy each component of Voltron's Infrastructure."""
+    ### OC Login
 
+    ### Infrastructure Deployment
+    KafkaDeployer.main()
+    ArangoDeployer.main()
+    InfluxDeployer.main()
+    OpenbmpDeployer.main()
+    TelemetryDeployer.main()
+    MDTDeployer.main()
 
-### OpenShift Login
-#call(["oc", "login", "https://10.200.99.44:8443"])
-oclogin = pexpect.spawn('oc login https://10.200.99.44:8443')
-oclogin.delaybeforesend = 1
-oclogin.expect('Username:')
-oclogin.sendline('admin')
-oclogin.expect('Password:')
-oclogin.sendline('admin')
-
-### Infrastructure Deployment
-KafkaDeployer.main()
-ArangoDeployer.main()
-InfluxDeployer.main()
-OpenbmpDeployer.main()
-TelemetryDeployer.main()
-MDTDeployer.main()
-
+if __name__ == '__main__':
+    main()
