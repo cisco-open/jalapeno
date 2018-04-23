7166f3d1d1d9a497a89ef99660aae820bc8a5cf4
ziausyed <ziausyed@cisco.com>
2018-04-22 22:50:18 -0400

    MVPZ: Infrastructure - Splitting Framework and Topology, Validating Kafka and Arango Deployment

--- infra/deploy_infrastructure.py
+++ infra/deploy_infrastructure.py
@@ -7,7 +7,7 @@ import arangodb.deploy_arango as ArangoDeployer
 import influxdb.deploy_influx as InfluxDeployer
 import openbmpd.deploy_openbmp as OpenbmpDeployer
 import telemetry.deploy_telemetry as TelemetryDeployer
-import mdt-stack.deploy_mdt as MDTDeployer
+#import mdt-stack.deploy_mdt as MDTDeployer
 
 def main():
     """Deploy each component of Voltron's Infrastructure."""
@@ -15,11 +15,12 @@ def main():
 
     ### Infrastructure Deployment
     KafkaDeployer.main()
+    exit(0)
     ArangoDeployer.main()
     InfluxDeployer.main()
     OpenbmpDeployer.main()
     TelemetryDeployer.main()
-    MDTDeployer.main()
+#    MDTDeployer.main()
 
 if __name__ == '__main__':
     main()
