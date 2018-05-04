a2a6888ddae66e0d8310cc694e6c61ce41598a67
ziausyed <ziausyed@cisco.com>
2018-05-04 08:32:08 -0400

    MVPZ: Services - Bandwidth Connection Fix

--- services/collectors/bandwidth/util/connections.py
+++ services/collectors/bandwidth/util/connections.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 """Database connection library.
-Currently includes connection capabilities for ArangoDB.
+Currently includes connection capabilities for ArangoDB and InfluxDB.
 """
 
 from pyArango.connection import Connection
+from influxdb import InfluxDBClient
 
 class ArangoConn():
     """Connection class for ArangoDB."""
@@ -16,4 +17,9 @@ class ArangoConn():
 	query_result = collection.AQLQuery(aql, rawResults = rawResults)
 	return query_result
 
+class InfluxConn():
+    """Connection class for InfluxDB."""
+    def connect_influx(self, host, port, user, password, dbname):
+        influx_client = InfluxDBClient(host, port, user, password, dbname)
+        return influx_client
 # other database connection capabilities can be added here
