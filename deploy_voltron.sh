90013d71df52d9f0657186a68ef0198804b5dc40
CentOS <zia>
2018-06-08 16:05:30 -0400

    MVPZ Adding timing corrections to telemetry deployment

--- deploy_voltron.sh
+++ deploy_voltron.sh
@@ -6,7 +6,6 @@ sh infra/deploy_infrastructure.sh
 sleep 30
 sh services/collectors/deploy_collectors.sh
 sh services/framework/deploy_framework.sh
-sleep 30
 sh infra/deploy_api.sh
 
 #sh services/responders/deploy_responders.sh
