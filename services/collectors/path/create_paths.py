4c88eeebc07e11e7ed4bf45f5f70e8afd2a118e1
ziausyed <ziausyed@cisco.com>
2018-10-31 17:02:16 -0400

    Converting path generation (hop-by-hop) to EPEPaths (egress points only)

--- services/collectors/path/create_paths.py
+++ services/collectors/path/create_paths.py
@@ -30,6 +30,7 @@ def main():
     while(True):
         generate_paths(database, collection)
 
+
 def create_collection(db):
     """Create new collection in ArangoDB.
     If the collection exists, connect to that collection.
@@ -44,88 +45,93 @@ def create_collection(db):
         collection = database[collection_name]
     return collection
 
+
 def generate_paths(db, collection):
     """Generate paths in a network using Arango data.
     Insert generated paths into the specified collection.
     """
     database = db
-    source = queryconfig.source  # the source for query purposes is vm-t0
-    vmsource = queryconfig.vmsource  # the source for client purposes is vm-vm0
     destination_list = open("configs/prefixes.txt").readlines()  # all prefixes should be listed in file
     for dest in destination_list:
         destination = dest.rstrip("\n\r")
-        print("\nGenerating all paths from " + vmsource + " to " + destination)
-        paths = generate_paths_query(database, source, destination)
+        #print("\n#############################################################################################################")
+        print("Generating all paths to " + destination)
+        #print("#############################################################################################################")
+        paths = generate_paths_query(database, destination)
         for path in paths:
-            create_path_record(collection, path, vmsource, destination)  # insert path into collection
-        clean_paths_collection(database, paths, vmsource, destination)
-    time.sleep(10)
+            create_path_record(collection, path, destination)  # insert path into collection
+        clean_paths_collection(database, paths, destination)
+    time.sleep(30)
 
-def generate_paths_query(db, source, destination):
+
+def generate_paths_query(db, destination):
     """AQL Query to generate paths from Arango data."""
-    aql = """ FOR v,e,p in 3
-        OUTBOUND @source
-        LinkEdgesV4, PrefixEdges
-        OPTIONS {bfs: False, uniqueEdges: "path", uniqueVertices: "path"}
-        FILTER p.vertices[-1]._id == @destination
-        RETURN [p.edges[* FILTER CURRENT.Label != null]._from, p.edges[* FILTER CURRENT.Label != null]._to,
-            p.edges[* FILTER CURRENT.Label != null].FromIP, p.edges[* FILTER CURRENT.Label != null].Label] """
-    bindVars = {'source': 'Routers/'+source, 'destination': 'Prefixes/'+destination}
+    aql = """FOR e in EPEEdges
+        FILTER e.Destination == @destination
+        RETURN {Source: e.Source, Destination: e.Destination, SRNodeSID: e.SourceSRNodeSID, Interface: e.SourceInterfaceIP, EPELabel: e.EPELabel} """
+    bindVars = {'destination': destination}
     paths = db.AQLQuery(aql, rawResults=True, bindVars=bindVars)
     return paths
 
-def clean_paths_collection(db, paths, source, destination):
+
+def clean_paths_collection(db, paths, destination):
     """Remove any paths in the Paths collection that do not exist in reality."""
+    print("Removing stale or broken paths to " + destination)
+    print("#############################################################################################################")
     real_paths = []
     for path in paths:
-        route = ', '.join(utilities.uniqify(path[0] + path[1])).replace('Routers/', '')  # formatting route and removing duplicates
-        key_route = route.replace(', ', "_to_")  # formatting document key
-        real_paths.append("Path:" + source + "_to_" + key_route + "_to_" + destination)
-    aql = """FOR p in Paths
+        egress_peer = path["Source"]
+        epe_label = path["EPELabel"]
+        path_destination = path["Destination"]
+        key = "EPEPath:" + egress_peer + "_" + epe_label + "_" + path_destination
+        real_paths.append(key)
+
+    aql = """FOR p in EPEPaths
         FILTER p.Destination == @destination
         RETURN p._key """
     bindVars = {'destination': destination}
     existing_path_collection = db.AQLQuery(aql, rawResults=True, bindVars=bindVars)
     for current_path in existing_path_collection:
         if current_path not in real_paths:
-            print("Path " + str(current_path) + " does not exist anymore. Removing from Paths collection.")
-            aql = """REMOVE @key IN Paths """
+            #print("EPEPath " + str(current_path) + " does not exist anymore. Removing from EPEPaths collection.")
+            aql = """REMOVE @key IN EPEPaths """
             bindVars = {'key': str(current_path)}
             db.AQLQuery(aql, rawResults=True, bindVars=bindVars)
+        #else:
+            #print(current_path + " exists, no need to remove")
+    #print("#############################################################################################################\n")
 
-
-def create_path_record(collection, path, source, destination):
+def create_path_record(collection, path, destination):
     """Create new path record and insert into collection.
     A sample path record has the following structure:
-        key: Path:10.1.2.1_to_10.1.1.0_to_10.1.1.2_to_10.1.1.4_to_10.100.100.8_to_10.13.0.0_24
-        "Latency": 0, "Label_Path": "24006_24007_24007", "Source": "10.1.2.1",
-        "Path": "10.1.1.0, 10.1.1.2, 10.1.1.4, 10.100.100.8", "Destination": "10.13.0.0_24",
-        "Interface_Path": "2.2.2.4, 2.2.2.12, 2.2.2.24"
+        key: Path:10.0.0.1_24014_71.71.8.0
+        "Egress_Peer": "10.0.0.1", Label_Path": "100003_24013"
+        "Interface": "2.2.71.0", "Destination": "71.71.8.0"
     """
-    route = ', '.join(utilities.uniqify(path[0] + path[1])).replace('Routers/', '')  # formatting route and removing duplicates
-    key_route = route.replace(', ', "_to_")  # formatting document key
-    interfaces = ', '.join(path[2])  # formatting path interfaces
-    label_stack = ', '.join(path[3]).replace(', ', "_")  # formatting label stack
-    print("Creating path: " + source + ", " + route + ", " + destination)
-
-    '''big question here. is it reasonable to say that from VM0 to C9, a path (t0, l1, p3, ep5) would be a singular path?
-       is there a chance that one path could have two sets of interfaces, labels, latencies?
-       for example, instead of interfaces 2.2.2.2, 2.2.2.2, 2.2.2.24, we get u'2.2.2.2', u'10.10.10.6', u'2.2.2.16
-       would the interface matter?
-    '''
+    egress_peer = path["Source"]
+    sr_node_sid = path["SRNodeSID"]
+    egress_interface = path["Interface"]
+    epe_label = path["EPELabel"]
+    path_destination = path["Destination"]
+    label_stack = sr_node_sid + "_" + epe_label
+    key = "EPEPath:" + egress_peer + "_" + epe_label + "_" + path_destination
+    #print("Creating path from egress-peer " + egress_peer + " through interface " + egress_interface + " to external prefix " + path_destination)
+    #print("Path key: " + key)
+    #print("Path label stack: " + label_stack)
+    
     try:
         document = collection.createDocument()
-        document["_key"] = "Path:" + source + "_to_" + key_route + "_to_" + destination
-        document["Source"] = source
-        document["Destination"] = destination
-        document["Path"] = str(route)
-        document["Interface_Path"] = str(interfaces)
+        document["_key"] = key 
+        document["Egress_Peer"] = egress_peer
+        document["Egress_Interface"] = egress_interface
         document["Label_Path"] = str(label_stack)
-        document["Latency"] = 0
+        document["Destination"] = destination
         document.save()
+        #print("Successfully created path")
     except CreationError:
-        print("That path already exists!")
+        #print("That path already exists!")
         pass
+    #print("#############################################################################################################")
 
 def setup_logging():
     logging.getLogger().setLevel(logging.INFO)
