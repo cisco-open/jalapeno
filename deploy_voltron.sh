970ab17390bb3fc0a89e1cbeb1d944d409791561
ziausyed <ziausyed@cisco.com>
2018-12-21 04:54:23 -0500

    Image pulling prior to deployment

--- deploy_voltron.sh
+++ deploy_voltron.sh
@@ -9,6 +9,12 @@ oc login https://localhost:8443 -u admin -p admin -n voltron
 oc new-project voltron --description=voltron --display-name=voltron
 sleep 3
 
+### Pulling images locally
+docker pull ievoltron/topology:2.1
+docker pull ievoltron/external-links-performance-collector:2.1.1
+docker pull ievoltron/internal-links-performance-collector:2.1.1
+docker pull ievoltron/api:0.0.1
+
 ### Deploying Infrastructure
 sh infra/deploy_infrastructure.sh
 sleep 20
