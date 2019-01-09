44de72e83e8455bc03991e22297ce123caa7f455
ziausyed <ziausyed@cisco.com>
2019-01-09 04:45:30 -0500

    Prelimarily setting up XE deployment within telemetry deployment, pending XR->XE config conversion

--- infra/telemetry/configure_telemetry.py
+++ infra/telemetry/configure_telemetry.py
@@ -35,13 +35,18 @@ def apply_telemetry_config(netmiko_host_dict, telemetry_config):
 def main():
     """Load the hosts and configure telemetry."""
     logging.basicConfig(level=logging.INFO)
-    telemetry_config = load_telemetry_config(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/config_xr')
+    xr_telemetry_config = load_telemetry_config(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/config_xr')
+    xe_telemetry_config = load_telemetry_config(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/config_xe')
     hosts = get_hosts(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/hosts.json')
     for host in hosts:
         network_host = host['netmiko_network']
+        device_type = network_host['device_type']
         logging.info('Configuring telemetry for %s', network_host['ip'])
         remove_telemetry_config(network_host)
-        apply_telemetry_config(network_host, telemetry_config)
+        if device_type == "cisco_xr":
+            apply_telemetry_config(network_host, xr_telemetry_config)
+        elif device_type == "cisco_xe":
+            apply_telemetry_config(network_host, xe_telemetry_config)
 
 if __name__ == '__main__':
     main()
