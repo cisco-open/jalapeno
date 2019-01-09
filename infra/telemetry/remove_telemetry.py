6a2beb34f6019cb3d276287d328b0e6a1e3e792d
ziausyed <ziausyed@cisco.com>
2019-01-09 02:30:40 -0500

    Adding ability to remove telemetry config

--- infra/telemetry/remove_telemetry.py
+++ infra/telemetry/remove_telemetry.py
@@ -2,11 +2,18 @@
 """Remove Telemetry streaming on host network devices.
 Unconfigures devices' currently telemetry configurations.
 """
+import logging, os
 import configure_telemetry
+from util import get_hosts
 
 def main():
     print("Unconfiguring telemetry on devices")
-    configure_telemetry.remove_telemetry_config()
+    hosts = get_hosts(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/hosts.json')
+    for host in hosts:
+        network_host = host['netmiko_network']
+        logging.info('Removing telemetry configuration for %s', network_host['ip'])
+        configure_telemetry.remove_telemetry_config(network_host)
+    exit()
 
 if __name__ == '__main__':
     main()
