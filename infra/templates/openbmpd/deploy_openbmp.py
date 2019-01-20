f880d61e75b5b86e9755344bd5787d890164f6cc
ziausyed <ziausyed@cisco.com>
2019-01-20 06:34:35 -0500

    Temporarily removing XE OpenBMP configuration

--- infra/templates/openbmpd/deploy_openbmp.py
+++ infra/templates/openbmpd/deploy_openbmp.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3.6
 """Deploy OpenBMP container locally.
 Receives OpenBMP data and forwards to Kafka."""
-import configure_openbmp
+#import configure_openbmp
 import os
 
 def main():
+    ### Assuming OpenBMP is configured on devices
     #print("Configuring OpenBMP on devices")
     #configure_openbmp.main()
+
     print("Deploying OpenBMP container on host")
     os.system("docker run -d --name=openbmp_collector -e KAFKA_FQDN={{ kafka_endpoint }} -v /var/openbmp/config:/config -p {{ openbmp_port }}:{{ openbmp_port }} openbmp/collector")
 
 if __name__ == '__main__':
     main()
-
