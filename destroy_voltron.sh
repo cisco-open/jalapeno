12e55c92a6b0893b8ea13a2f7104b49e1c32fb02
ziausyed <ziausyed@cisco.com>
2019-07-14 19:29:35 -0400

    2.2 Upgrades

--- destroy_voltron.sh
+++ destroy_voltron.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
-sudo make clean
+echo "Logging into OpenShift"
+oc login https://localhost:8443 -u admin -p admin -n voltron
+
+echo "Shutting down Voltron"
+oc delete project voltron
+
+echo "Please wait"
+sleep 60
+
+echo "Deleting Persistent Volumes"
+oc delete pv arangodb
+oc delete pv arangodb-apps
+oc delete pv pvkafka
+oc delete pv pvzoo
+
+echo "Removing data logs"
+sudo rm -rf /export/arangodb/*
+sudo rm -rf /export/arangodb-apps/_db
+sudo rm -rf /export/pvkafka/topics
+sudo rm -rf /export/pvzoo/{data,log}
+
+echo "Deleting OpenBMP and stopping Telemetry"
 sh infra/destroy_infrastructure.sh
--- services/collectors/internal-links-performance/telemetry_interfaces.py
+++ services/collectors/internal-links-performance/telemetry_interfaces.py
@@ -1,14 +1,10 @@
 """This maps telemetry derived internal interfaces with router IPs and interface IP addresses. In the future, this should be automated."""
 telemetry_interface_mapper = {}
-telemetry_interface_mapper['10.0.0.0_10.1.1.0'] = ['r0.622', 'GigabitEthernet0/0/0/3'] 
-telemetry_interface_mapper['10.0.0.0_10.1.1.2'] = ['r0.622', 'GigabitEthernet0/0/0/4'] 
-telemetry_interface_mapper['10.0.0.0_10.1.1.8'] = ['r0.622', 'GigabitEthernet0/0/0/5'] 
-telemetry_interface_mapper['10.0.0.0_10.1.1.10'] = ['r0.622', 'GigabitEthernet0/0/0/6'] 
+telemetry_interface_mapper['10.0.0.0_10.1.1.0'] = ['ORD_TR00.6.3.3', 'GigabitEthernet0/0/0/3'] 
+telemetry_interface_mapper['10.0.0.0_10.1.1.2'] = ['ORD_TR00.6.3.3', 'GigabitEthernet0/0/0/4'] 
 
-telemetry_interface_mapper['10.0.0.1_10.1.1.1'] = ['r1.622', 'GigabitEthernet0/0/0/0']
-telemetry_interface_mapper['10.0.0.1_10.1.1.23'] = ['r1.622', 'GigabitEthernet0/0/0/4']
-telemetry_interface_mapper['10.0.0.1_10.1.1.35'] = ['r1.622', 'GigabitEthernet0/0/0/3']
+telemetry_interface_mapper['10.0.0.1_10.1.1.1'] = ['ORD_PR01', 'GigabitEthernet0/0/0/0']
+telemetry_interface_mapper['10.0.0.1_10.1.1.5'] = ['ORD_PR01', 'GigabitEthernet0/0/0/5']
 
-telemetry_interface_mapper['10.0.0.2_10.1.1.3'] = ['r2.622', 'GigabitEthernet0/0/0/0']
-telemetry_interface_mapper['10.0.0.2_10.1.1.25'] = ['r2.622', 'GigabitEthernet0/0/0/4']
-telemetry_interface_mapper['10.0.0.2_10.1.1.37'] = ['r2.622', 'GigabitEthernet0/0/0/3']
+telemetry_interface_mapper['10.0.0.2_10.1.1.3'] = ['ORD_PR02', 'GigabitEthernet0/0/0/0']
+telemetry_interface_mapper['10.0.0.2_10.1.1.7'] = ['ORD_PR02', 'GigabitEthernet0/0/0/5']
