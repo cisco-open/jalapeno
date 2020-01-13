ac9355252863a0500adbfc393d3c6ca4eeb025bc
ziausyed <ziausyed@cisco.com>
2020-01-13 02:02:05 -0500

    Deployment script

--- deploy_voltron.sh
+++ deploy_voltron.sh
@@ -9,12 +9,12 @@ oc login https://localhost:8443 -u admin -p admin -n voltron
 oc new-project voltron --description=voltron --display-name=voltron
 sleep 3
 
-### Pulling images locally, uncomment if necessary
-#docker pull ievoltron/topology:2.1.2
-#docker pull ievoltron/external-links-performance-collector:2.1.1
-#docker pull ievoltron/internal-links-performance-collector:2.1.1
-#docker pull ievoltron/api:0.0.1.2
-#docker pull ievoltron/portal:0.0.1
+### Pulling images locally, update image version as necessary
+docker pull ievoltron/topology:2.1.2
+docker pull ievoltron/external-links-performance-collector:2.1.1
+docker pull ievoltron/internal-links-performance-collector:2.1.1
+docker pull ievoltron/api:0.0.1.2
+docker pull ievoltron/portal:0.0.1
 
 ### Deploying Infrastructure
 sh infra/deploy_infrastructure.sh
