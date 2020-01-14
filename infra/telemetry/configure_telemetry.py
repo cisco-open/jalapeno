60b2bda0a697a3f44c581f4743473d66350673f6
ziausyed <ziausyed@cisco.com>
2020-01-14 13:39:00 -0500

    Adding password capabilities to manage different device passwords

--- infra/telemetry/configure_telemetry.py
+++ infra/telemetry/configure_telemetry.py
@@ -5,10 +5,10 @@ from util import get_hosts
 from jinja2 import *
 from configs import pipeline_config
 
-def retrieve_device_info(host_ip, device_type):
+def retrieve_device_info(host_ip, device_type, password):
     logging.info('Retrieving hostname/loopback for %s', host_ip)
     driver = napalm.get_network_driver(device_type)
-    device = driver(hostname=host_ip, username='cisco', password='cisco')
+    device = driver(hostname=host_ip, username='cisco', password=password)
     device.open()
     interface_dict = device.get_interfaces_ip()
     hostname = device.get_facts()['hostname']
@@ -29,10 +29,10 @@ def render_telemetry_config(device_type, hostname, loopback, pipeline_ip, pipeli
     with open(telemetry_config, "w") as file_handler:
         file_handler.write(outputText)
 
-def deploy_telemetry_config(device_type, hostname, host_ip):
+def deploy_telemetry_config(device_type, hostname, host_ip, password):
     logging.info('Deploying %s telemetry config for %s at %s', device_type, hostname, host_ip)
     driver = napalm.get_network_driver(device_type)
-    device = driver(hostname=host_ip, username='cisco', password='cisco')
+    device = driver(hostname=host_ip, username='cisco', password=password)
     device.open()
     dirname = os.path.dirname(os.path.abspath(__file__))
     config_file = dirname + '/configs/' + device_type + '/' + hostname
@@ -52,14 +52,14 @@ def main():
     hosts = get_hosts(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/hosts.json')
     for host in hosts:
         network_host = host['netmiko_network']
-        host_ip, device_type = network_host['ip'], network_host['device_type']
+        host_ip, device_type, password = network_host['ip'], network_host['device_type'], network_host['password']
         logging.info('Configuring telemetry for %s', host_ip)
-        hostname, loopback = retrieve_device_info(host_ip, device_type)
+        hostname, loopback = retrieve_device_info(host_ip, device_type, password)
         if device_type == 'ios':
             render_telemetry_config(device_type, hostname, loopback, pipeline_ip, pipeline_port, xe_template)
         elif device_type == 'iosxr':
             render_telemetry_config(device_type, hostname, loopback, pipeline_ip, pipeline_port,  xr_template)
-        deploy_telemetry_config(device_type, hostname, host_ip)
+        deploy_telemetry_config(device_type, hostname, host_ip, password)
         logging.info('Configured telemetry for %s!', host_ip)
         print()
 
