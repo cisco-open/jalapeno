e4057fdd9b400d6df972235c424a06a424e4ca70
ziausyed <ziausyed@cisco.com>
2020-01-14 13:33:37 -0500

    OpenBMP deployment script

--- infra/openbmpd/deploy_openbmp.py
+++ infra/openbmpd/deploy_openbmp.py
@@ -6,8 +6,10 @@ import os
 
 def main():
     ### Assuming OpenBMP is configured on devices
-    #print("Configuring OpenBMP on devices")
-    #configure_openbmp.main()
+
+    # Skipping configuration of OpenBMP on devices, uncomment if necessary
+    # print("Configuring OpenBMP on devices")
+    # configure_openbmp.main()
 
     print("Deploying OpenBMP container on host")
     os.system("docker run -d --name=openbmp_collector -e KAFKA_FQDN=10.0.250.2:30902 -v /var/openbmp/config:/config -p 5000:5000 openbmp/collector")
