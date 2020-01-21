297c19207dce2bbc115024d2dcaf8e68b5ccba2c
ziausyed <ziausyed@cisco.com>
2020-01-20 21:14:14 -0800

    Renaming Voltron to Jalapeno

--- docs/testbed_virtual_network_setup.sh
+++ docs/testbed_virtual_network_setup.sh
@@ -21,7 +21,7 @@ ip route add 10.0.130.0/24 via 10.0.254.71 dev vlt_outside_br
 ip route add 10.0.131.0/24 via 10.0.254.76 dev vlt_outside_br
 sleep 1
 
-echo adding iptables NAT entries so voltron k8s containers may be reached from the outside world
+echo adding iptables NAT entries so jalapeno k8s containers may be reached from the outside world
 # openshift UI
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 8443 -j DNAT --to-destination 10.251.251.250:8443
 
@@ -42,6 +42,6 @@ iptables -t nat -A POSTROUTING -o vlt_mgt_br -j MASQUERADE
 # source NAT for external traffic inbound to virtual topology
 iptables -t nat -A POSTROUTING -o vlt_outside_br -j MASQUERADE
 
-# optional: NAT all outbound traffic from voltron/the server itself.  Uncomment if needed
+# optional: NAT all outbound traffic from jalapeno/the server itself.  Uncomment if needed
 # iptables -t nat -A POSTROUTING -o <server_outside_nic> -j MASQUERADE
 
