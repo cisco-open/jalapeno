9e875ce06f3f9e3555a4f0f551bd6b25913cbaef
ziausyed <ziausyed@cisco.com>
2020-01-22 10:13:41 -0800

    Renaming Voltron to Jalapeno

--- services/api/src/swagger_server/__main__.py
+++ services/api/src/swagger_server/__main__.py
@@ -9,7 +9,7 @@ from swagger_server import encoder
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Voltron API'})
+    app.add_api('swagger.yaml', arguments={'title': 'Jalapeno API'})
     CORS(app.app)
     app.run(port=8000)
 
