d7589f4becd5e1f2729f9177f0bd26d01a7905db
ziausyed <ziausyed@cisco.com>
2020-01-22 10:00:37 -0800

    Renaming Voltron to Jalapeno

--- services/api/src/setup.py
+++ services/api/src/setup.py
@@ -18,10 +18,10 @@ REQUIRES = ["connexion"]
 setup(
     name=NAME,
     version=VERSION,
-    description="Voltron API",
-    author_email="voltron-team@cisco.com",
+    description="Jalapeno API",
+    author_email="jalapeno-team@cisco.com",
     url="",
-    keywords=["Swagger", "Voltron API"],
+    keywords=["Swagger", "Jalapeno API"],
     install_requires=REQUIRES,
     packages=find_packages(),
     package_data={'': ['swagger/swagger.yaml']},
