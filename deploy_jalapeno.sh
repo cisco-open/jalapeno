f16bd2062fada7b4101aeb37fba33eeccd2c84f6
ziausyed <ziausyed@cisco.com>
2020-01-27 01:33:58 -0500

    Jalapeno transition
    Former-commit-id: 199c55ef0dfb962dcec350e6377ad68f43927f29

--- deploy_jalapeno.sh
+++ deploy_jalapeno.sh
@@ -10,22 +10,22 @@ oc new-project jalapeno --description=jalapeno --display-name=jalapeno
 sleep 3
 
 ### Pulling images locally, update image version as necessary. Must be part of https://hub.docker.com/orgs/iejalapeno to pull images -- alternatively, build and upload images to personal image repos.
-docker pull iejalapeno/topology:2.1.2
-docker pull iejalapeno/external-links-performance-collector:2.1.1
-docker pull iejalapeno/internal-links-performance-collector:2.1.1
-docker pull iejalapeno/api:0.0.1.2
-docker pull iejalapeno/portal:0.0.1
+#docker pull iejalapeno/topology:2.1.2
+#docker pull iejalapeno/external-links-performance-collector:2.1.1
+#docker pull iejalapeno/internal-links-performance-collector:2.1.1
+#docker pull iejalapeno/api:0.0.1.2
+#docker pull iejalapeno/portal:0.0.1
 
 ### Deploying Infrastructure
 sh infra/deploy_infrastructure.sh
 sleep 20
 
 ### Deploying Services (vCollectors)
-sh services/collectors/deploy_collectors.sh
+#sh services/collectors/deploy_collectors.sh
 sleep 20
 
 ### Deploying API
-sh services/api/deploy_api.sh
+#sh services/api/deploy_api.sh
 
 ### Deploying Portal
 #sh services/portal/deploy_portal.sh
