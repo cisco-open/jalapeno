6b430c4556eeb9125c0a6b1ff7bf060a22f04e6e
skkumaravel <srirudrankumaravel@gmail.com>
2020-02-21 10:29:44 -0800

    Update naming
    Former-commit-id: 0ab795233e31c51422c0d67cc00b4ed25a985fd1

---
+++ collectors/deploy_collectors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+echo "Starting deployment of telemetry services"
+KUBE=microk8s.kubectl
+
+echo "Creating jalapeno-telemetry Namespace"
+${KUBE} create -f ${PWD}/${BASEDIR}/namespace-jalapeno-collectors.json
+
+echo "Deploying Pipeline Ingress"
+${KUBE} create -f ${PWD}/${BASEDIR}/pipeline-ingress/.
+
+echo "Deploying Openbmpd Collector"
+${KUBE} create -f ${PWD}/${BASEDIR}/openbmpd/.
+
+echo "Finished deploying telemetry services"
+echo "Next configure routers!"
