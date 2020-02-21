42df96a3f58732dbcc4ef920f98d3bc17ac4a29f
skkumaravel <srirudrankumaravel@gmail.com>
2020-02-21 10:19:30 -0800

    Fix naming

--- services/collectors/deploy_collectors.sh
+++ services/collectors/deploy_collectors.sh
@@ -4,6 +4,6 @@ KUBE=microk8s.kubectl
 
 ${KUBE} create -f ${PWD}/${BASEDIR}/topology/topology_dp.yaml
 ${KUBE} create -f ${PWD}/${BASEDIR}/epe-edges/epe_edges_collector_dp.yaml
-${KUBE} create -f ${PWD}/${BASEDIR}/epe-paths/epe_paths_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/epe-paths/paths_collector_dp.yaml
 ${KUBE} create -f ${PWD}/${BASEDIR}/external-links-performance/external_links_performance_collector_dp.yaml
 ${KUBE} create -f ${PWD}/${BASEDIR}/internal-links-performance/internal_links_performance_collector_dp.yaml
