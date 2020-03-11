889efafe8fb588041e20b821e30cb23a698d781f
ziausyed <ziausyed@cisco.com>
2020-03-11 10:25:25 -0700

    Restructuring Jalapeno
    Former-commit-id: 9502e8b8792c8a49709414c6d575ae456e77e7d3

---
+++ processors/deploy_processors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+KUBE=$1
+if [ -z "$1" ]
+  then
+    KUBE=kubectl
+fi
+
+${KUBE} create -f ${PWD}/${BASEDIR}/topology/topology_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/epe-edges/epe_edges_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/epe-paths/paths_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/external-links-performance/external_links_performance_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/internal-links-performance/internal_links_performance_collector_dp.yaml
