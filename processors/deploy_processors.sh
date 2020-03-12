3f65aa36767bce97456a02e516b86986f6706525
ziausyed <ziausyed@cisco.com>
2020-03-11 22:31:14 -0700

    Updates for Jalapeno migration to k8s
    Former-commit-id: f9cb5c4a28e473afdd51cce87cb2e6034d153992
    Former-commit-id: 198932b4f9125b17778d329b6543e7321855c5e7

--- processors/deploy_processors.sh
+++ processors/deploy_processors.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]
 fi
 
 ${KUBE} create -f ${PWD}/${BASEDIR}/topology/topology_dp.yaml
-#${KUBE} create -f ${PWD}/${BASEDIR}/epe-edges/epe_edges_collector_dp.yaml
-#${KUBE} create -f ${PWD}/${BASEDIR}/epe-paths/paths_collector_dp.yaml
-#${KUBE} create -f ${PWD}/${BASEDIR}/external-links-performance/external_links_performance_collector_dp.yaml
-#${KUBE} create -f ${PWD}/${BASEDIR}/internal-links-performance/internal_links_performance_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/epe-edges/epe_edges_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/epe-paths/paths_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/external-links-performance/external_links_performance_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/internal-links-performance/internal_links_performance_collector_dp.yaml
