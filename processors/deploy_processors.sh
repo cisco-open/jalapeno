7c5c5723b3b67b12858fb3e2c6eec14708229553
Zia Syed <ziausyed@cisco.com>
2020-03-30 15:49:18 -0700

    Update deploy_processors.sh

--- processors/deploy_processors.sh
+++ processors/deploy_processors.sh
@@ -9,7 +9,4 @@ fi
 ${KUBE} create -f ${PWD}/${BASEDIR}/topology/topology_dp.yaml
 ${KUBE} create -f ${PWD}/${BASEDIR}/ls/ls_dp.yaml
 ${KUBE} create -f ${PWD}/${BASEDIR}/l3vpn/l3vpn_dp.yaml
-${KUBE} create -f ${PWD}/${BASEDIR}/epe-edges/epe_edges_collector_dp.yaml
-${KUBE} create -f ${PWD}/${BASEDIR}/epe-paths/paths_collector_dp.yaml
-${KUBE} create -f ${PWD}/${BASEDIR}/external-links-performance/external_links_performance_collector_dp.yaml
-${KUBE} create -f ${PWD}/${BASEDIR}/internal-links-performance/internal_links_performance_collector_dp.yaml
+${KUBE} create -f ${PWD}/${BASEDIR}/ls-performance/ls_performance_dp.yaml
