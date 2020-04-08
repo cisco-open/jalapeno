e4ff66a59fa0f21611a4d429e86ca3249a51d016
Zia Syed <ziausyed@cisco.com>
2020-04-08 02:34:02 -0700

    Patching in fixes from test deployments

--- deploy_jalapeno.sh
+++ deploy_jalapeno.sh
@@ -4,9 +4,11 @@
 
 ### Deploying Infrastructure
 sh infra/deploy_infrastructure.sh $1
+sleep 30
 
 ### Deploying Collectors
 sh collectors/deploy_collectors.sh $1
+sleep 30
 
 ### Deploying Processors
 sh processors/deploy_processors.sh $1
