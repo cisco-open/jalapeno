a4e843e9605c098a738ec093a469cd8878dff60c
ziausyed <ziausyed@cisco.com>
2020-05-01 13:30:43 -0700

    Creating GoBMP configuration utility

--- collectors/gobmp-util/configure_gobmp.py
+++ collectors/gobmp-util/configure_gobmp.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-"""Configure OpenBMP on IOS-XR / IOS-XE host devices."""
+"""Configure GoBMP on IOS-XR / IOS-XE host devices."""
 import logging, os, sys, napalm
 from util import get_hosts
 from jinja2 import *
 
-def deploy_openbmp_config(device_type, host_ip):
+def deploy_gobmp_config(device_type, host_ip):
     logging.info('Deploying %s telemetry config at %s', device_type, host_ip)
     driver = napalm.get_network_driver(device_type)
     device = driver(hostname=host_ip, username='cisco', password='cisco')
     device.open()
     dirname = os.path.dirname(os.path.abspath(__file__))
     if device_type == 'ios':
-        config_file = dirname + '/openbmp_config_xe'
+        config_file = dirname + '/gobmp_config_xe'
     elif device_type == 'iosxr':
-        config_file = dirname + '/openbmp_config_xr'
+        config_file = dirname + '/gobmp_config_xr'
     device.load_merge_candidate(filename=config_file)
     device.commit_config()
     device.close()
@@ -25,9 +25,9 @@ def main():
     for host in hosts:
         network_host = host['netmiko_network']
         host_ip, device_type = network_host['ip'], network_host['device_type']
-        logging.info('Configuring OpenBMP on %s', host_ip)
+        logging.info('Configuring GoBMP on %s', host_ip)
         deploy_openbmp_config(device_type, host_ip)
-        logging.info('Configured OpenBMP on %s!', host_ip)
+        logging.info('Configured GoBMP on %s!', host_ip)
         print()
 
 if __name__ == '__main__':
