910d51acbbfa306d24aa9e32415bef7e9918ea7f
ziausyed <ziausyed@cisco.com>
2020-06-02 02:13:43 -0700

    Uncommenting deployment script

--- deploy_jalapeno.sh
+++ deploy_jalapeno.sh
@@ -3,12 +3,12 @@
 ### Deploying Jalapeno ###
 
 ### Deploying Infrastructure
-#sh infra/deploy_infrastructure.sh $1
-#sleep 30
+sh infra/deploy_infrastructure.sh $1
+sleep 30
 
 ### Deploying Collectors
-#sh collectors/deploy_collectors.sh $1
-#sleep 30
+sh collectors/deploy_collectors.sh $1
+sleep 30
 
 ### Deploying Processors
 sh processors/deploy_processors.sh $1
