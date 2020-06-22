ce4ff5e1ec2b9cd518c285be86d9ade7ec7d7497
ziausyed <ziausyed@cisco.com>
2020-06-22 02:06:42 -0700

    Initial cleanup
    Former-commit-id: 2397a424370a2daee10c808585c2e8286c3615a3
    Former-commit-id: 74ed5ccf0a0e47255712b00e86a63e8460caf11c
    Former-commit-id: 907854c2bf71056951fa7039f2f93acd0217547a
    Former-commit-id: 839b6ebea29241862c17002d28e956667aaa588e
    Former-commit-id: 3e1df89dfd52b075cf4b2124031d31cb12cdc278
    Former-commit-id: 8869769fbb436bc822bd6ecb10cde4087bf1d1c6
    Former-commit-id: 005d908d58e256cd3f9958c3baefb2873f56bc0f
    Former-commit-id: 07ba23b0f652a90b86a307294d13c9ac6590856d

--- collectors/deploy_collectors.sh
+++ collectors/deploy_collectors.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-echo "Starting deployment of telemetry services"
+
 KUBE=$1
 if [ -z "$1" ]
   then
     KUBE=kubectl
 fi
 
-echo "Creating jalapeno-telemetry Namespace"
+echo "Deploying Jalapeno Collectors"
+
+echo "Creating jalapeno-collectors namespace"
 ${KUBE} create -f ${PWD}/${BASEDIR}/namespace-jalapeno-collectors.json
 
-echo "Deploying Telegraf Ingress"
+echo "Deploying Telegraf-Ingress Collector to collect network performance-metric data"
 ${KUBE} create -f ${PWD}/${BASEDIR}/telegraf-ingress/.
 
-echo "Deploying GoBMP Collector"
+echo "Deploying GoBMP Collector to collect network topology data"
 ${KUBE} create -f ${PWD}/${BASEDIR}/gobmp/.
 
-echo "Finished deploying telemetry services"
-echo "Next configure routers!"
+echo "Finished deploying Jalapeno Collectors"
