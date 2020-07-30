585d7848b2eee759122c6584e02cc5dc46ea97e6
Bruce McDougall <brmcdoug@cisco.com>
2020-07-30 16:49:17 -0700

    l3vpn arango config

--- processors/l3vpn/configs/arangoconfig.py
+++ processors/l3vpn/configs/arangoconfig.py
@@ -2,8 +2,8 @@
 """Configuration file for ArangoDB connections.
 Specify the values according to environment.
 """
-#url = 'http://arango-np.jalapeno:8529'
+url = 'http://arango-np.jalapeno:8529'
 username = 'root'
 password = 'jalapeno'
 database = 'jalapeno'
-url = 'http://10.200.99.7:30852'
+#url = 'http://10.200.99.3:30852'
