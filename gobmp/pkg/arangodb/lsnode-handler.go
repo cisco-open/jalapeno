bd520e9f517e11b77b5ce879a54a8faa2394ed6e
Miroslav Los <mirlos@cisco.com>
2020-11-11 16:24:10 +0100

    Make area_id zero for IS-IS in LSNode key

--- gobmp/pkg/arangodb/lsnode-handler.go
+++ gobmp/pkg/arangodb/lsnode-handler.go
@@ -3,6 +3,7 @@ package arangodb
 import (
 	"strconv"
 
+	"github.com/sbezverk/gobmp/pkg/base"
 	"github.com/sbezverk/gobmp/pkg/message"
 )
 
@@ -11,8 +12,11 @@ type lsNodeArangoMessage struct {
 }
 
 func (n *lsNodeArangoMessage) MakeKey() string {
-
+	area_id := "0"
+	if (n.ProtocolID == base.OSPFv2 || n.ProtocolID == base.OSPFv3) {
+		area_id = n.OSPFAreaID
+	}
 	// The LSNode Key uses ProtocolID, DomainID, and OSPF Area ID (if OSPF is running)
 	// to create unique Keys for DB entries in multi-area / multi-topology scenarios
-	return strconv.Itoa(int(n.ProtocolID)) + "_" + strconv.Itoa(int(n.DomainID)) + "_" + n.OSPFAreaID + "_" + n.IGPRouterID
+	return strconv.Itoa(int(n.ProtocolID)) + "_" + strconv.Itoa(int(n.DomainID)) + "_" + area_id + "_" + n.IGPRouterID
 }
