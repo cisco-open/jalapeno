5268663dd2f73afc1a238b1f10d889f29ffe6e5b
brmcdoug <brmcdoug@cisco.com>
2021-01-31 19:43:15 -0800

    lslink mods

--- gobmp/go.mod
+++ gobmp/go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/Shopify/sarama v1.27.0
 	github.com/arangodb/go-driver v0.0.0-20200831144536-17278d36b7e8
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/sbezverk/gobmp v0.0.1-beta.0.20210118182608-9eb4600cfbbc
+	github.com/sbezverk/gobmp v0.0.1-beta.0.20210125022733-d2f3a99d56ad
 	go.uber.org/atomic v1.7.0
 )
--- gobmp/pkg/arangodb/lslink-handler.go
+++ gobmp/pkg/arangodb/lslink-handler.go
@@ -3,6 +3,7 @@ package arangodb
 import (
 	"strconv"
 
+	"github.com/sbezverk/gobmp/pkg/base"
 	"github.com/sbezverk/gobmp/pkg/message"
 )
 
@@ -20,6 +21,9 @@ func (l *lsLinkArangoMessage) MakeKey() string {
 		localID = l.LocalLinkIP
 		remoteID = l.RemoteLinkIP
 	} else {
+		// If Local/Remote IP are missing as in the case of unnumbered links,
+		// then links are identified by Local/Remote Link IDs which are presented in the inverse dotted notation
+		// Example: Link ID 14 is presented as 0.0.0.14
 		localID = strconv.Itoa(int(l.LocalLinkID>>24)&0x000000ff) + "." +
 			strconv.Itoa(int(l.LocalLinkID>>16)&0x000000ff) + "." +
 			strconv.Itoa(int(l.LocalLinkID>>8)&0x000000ff) + "." +
@@ -30,7 +34,15 @@ func (l *lsLinkArangoMessage) MakeKey() string {
 			strconv.Itoa(int(l.RemoteLinkID)&0x000000ff)
 	}
 
+	routerID := l.IGPRouterID
+	remoteRouterID := l.RemoteIGPRouterID
+	// If Protocol ID == 7 (BGP) LS Link does not carry IGP Router ID field, instead BGP Router ID must be used
+	if l.ProtocolID == base.BGP {
+		routerID = l.BGPRouterID
+		remoteRouterID = l.BGPRemoteRouterID
+	}
+
 	// The LSLink Key uses ProtocolID, DomainID, and Multi-Topology ID
 	// to create unique Keys for DB entries in multi-area / multi-topology scenarios
-	return strconv.Itoa(int(l.ProtocolID)) + "_" + strconv.Itoa(int(l.DomainID)) + "_" + strconv.Itoa(mtid) + "_" + l.AreaID + "_" + l.IGPRouterID + "_" + localID + "_" + l.RemoteIGPRouterID + "_" + remoteID
+	return strconv.Itoa(int(l.ProtocolID)) + "_" + strconv.Itoa(int(l.DomainID)) + "_" + strconv.Itoa(mtid) + "_" + l.AreaID + "_" + routerID + "_" + localID + "_" + remoteRouterID + "_" + remoteID
 }
