babaf9a2c7f7ee5c0478c077068251f7d317d1d5
brmcdoug <brmcdoug@cisco.com>
2021-02-05 10:11:00 -0800

    ls edge deploy

--- processors/deploy_processors.sh
+++ processors/deploy_processors.sh
@@ -11,3 +11,6 @@ ${KUBE} create -f ${PWD}/${BASEDIR}/topology/topology.yaml
 
 echo "Deploying Telegraf-Egress"
 ${KUBE} create -f ${PWD}/${BASEDIR}/telegraf-egress/.
+
+echo "Deploying LS Edge Processor"
+${KUBE} create -f ${PWD}/${BASEDIR}/ls-edge-processor/ls-edge-processor.yaml
