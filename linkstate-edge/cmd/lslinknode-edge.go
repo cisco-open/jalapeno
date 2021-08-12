02f846fafdaf21c6bd69153fea27d9dd6d498c41
brmcdoug <brmcdoug@cisco.com>
2021-08-12 15:13:58 -0700

    update with creds

--- linkstate-edge/cmd/lslinknode-edge.go
+++ linkstate-edge/cmd/lslinknode-edge.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"os"
 	"os/signal"
 	"runtime"
@@ -11,6 +13,17 @@ import (
 	"github.com/jalapeno/lslinknode-edge/pkg/kafkamessenger"
 
 	_ "net/http/pprof"
+)
+
+const (
+	// userFile defines the name of file containing base64 encoded user name
+	userFile = "./credentials/.username"
+	// passFile defines the name of file containing base64 encoded password
+	passFile = "./credentials/.password"
+	// MAXUSERNAME defines maximum length of ArangoDB user name
+	MAXUSERNAME = 256
+	// MAXPASS defines maximum length of ArangoDB password
+	MAXPASS = 256
 )
 
 var (
@@ -61,6 +74,13 @@ func main() {
 
 	// TODO (sbezverk) pass vertex collection type and edge collection type are parameters
 
+	// validateDBCreds check if the user name and the password are provided either as
+	// command line parameters or via files. If both are provided command line parameters
+	// will be used, if neither, processor will fail.
+	if err := validateDBCreds(); err != nil {
+		glog.Errorf("failed to validate the database credentials with error: %+v", err)
+		os.Exit(1)
+	}
 	dbSrv, err := arangodb.NewDBSrvClient(dbSrvAddr, dbUser, dbPass, dbName, vertexCollection, edgeCollection)
 	if err != nil {
 		glog.Errorf("failed to initialize databse client with error: %+v", err)
@@ -90,4 +110,48 @@ func main() {
 	dbSrv.Stop()
 
 	os.Exit(0)
+}
+
+func validateDBCreds() error {
+	// Attempting to access username and password files.
+	u, err := readAndDecode(userFile, MAXUSERNAME)
+	if err != nil {
+		if dbUser != "" && dbPass != "" {
+			return nil
+		}
+		return fmt.Errorf("failed to access %s with error: %+v and no username and password provided via command line arguments", userFile, err)
+	}
+	p, err := readAndDecode(passFile, MAXPASS)
+	if err != nil {
+		if dbUser != "" && dbPass != "" {
+			return nil
+		}
+		return fmt.Errorf("failed to access %s with error: %+v and no username and password provided via command line arguments", passFile, err)
+	}
+	dbUser, dbPass = u, p
+
+	return nil
+}
+
+func readAndDecode(fn string, max int) (string, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	l, err := f.Stat()
+	if err != nil {
+		return "", err
+	}
+	b := make([]byte, int(l.Size()))
+	n, err := io.ReadFull(f, b)
+	if err != nil {
+		return "", err
+	}
+	if n > max {
+		return "", fmt.Errorf("length of data %d exceeds maximum acceptable length: %d", n, max)
+	}
+	b = b[:n]
+
+	return string(b), nil
 }
