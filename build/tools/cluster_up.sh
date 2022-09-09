67403ea419d36235568eb2d08639e812402c922a
Jeff Byzek <byzek@cisco.com>
2022-09-09 10:22:52 -0400

    Sanity testing for k8s (#57)
    * Minimal spinup action
    
    * Spindown/spinup test
    
    * Spindown/spinup test take two
    
    * Removing dummy action
    
    * master --> main

---
+++ build/tools/cluster_up.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KUBE=$1
+if [ -z "$1" ]
+  then
+    KUBE=kubectl
+fi
+
+TEST=`which $KUBE`
+if [ "$?" -eq 1 ]; then
+    echo "$KUBE not found, exiting..."
+    exit 1
+fi
+
+ATTEMPTS=0
+TIMEOUT=5
+until $KUBE get all -A | grep -o '[0-9]\/[0-9]' | grep -v '0\/1'
+do
+  ((ATTEMPTS=ATTEMPTS+1))
+  if [ $ATTEMPTS -gt 12]; then
+    echo "Cluster failed to come up"
+    exit 1
+  fi
+  sleep $TIMEOUT
+done
