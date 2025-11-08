5a07be4d56ef385114624a5a7a03aa6db26ccb38
brmcdoug <brmcdoug@cisco.com>
2025-11-08 14:52:58 -0800

    deploy

--- install/processors/deploy_processors.sh
+++ install/processors/deploy_processors.sh
@@ -14,3 +14,9 @@ ${KUBE} create -f ${PWD}/${BASEDIR}/telegraf-egress/.
 
 echo "Deploying Linkstate Edge Processor"
 ${KUBE} create -f ${PWD}/${BASEDIR}/linkstate-edge/linkstate-edge.yaml
+
+echo "Deploying IGP Graph Processor"
+${KUBE} create -f ${PWD}/${BASEDIR}/igp-graph/igp-graph.yaml
+
+echo "Deploying IP Graph Processor"
+${KUBE} create -f ${PWD}/${BASEDIR}/ip-graph/ip-graph.yaml
